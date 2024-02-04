b1937895a97fcc989f78cb1e22d276d6ef704111
NyarlAzathoth <92152241+NyarlAzathoth@users.noreply.github.com>
2024-02-04 20:50:27 +0100

    Completed exercises 3-7 successfully

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let length = string.length;
+    let stringReversed = '';
 
+    for (let i = 0; i<length; i++) {
+        stringReversed += string.charAt(length-1-i);
+    }
+
+    return stringReversed
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...args) {
+    let newList = [...list];
+    let numOfSplices = 0;
 
+    for (let i=0; i<list.length; i++) {
+        for (let j=0; j<args.length; j++) {
+            if (list[i] === args[j]) {
+                newList.splice(i-numOfSplices, 1);
+                numOfSplices++;
+            }
+        }
+    }
+
+    return newList;
 };
 
 // Do not edit below this line
